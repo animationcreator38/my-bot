15e38ae3d545c017716046542c8332f59452de60
Kishan484 <121420677+Kishan484@users.noreply.github.com>
2023-05-22 11:43:26 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,7 +1,7 @@
 from os import environ
 
-API_ID = int(environ.get('API_ID', ''))
-API_HASH = environ.get('API_HASH', '')
+API_ID = int(environ.get('API_ID', '28103752'))
+API_HASH = environ.get('API_HASH', '479579601469809b9a9c8ffc15fbe11a')
 BOT_TOKEN = environ.get('BOT_TOKEN', '')
 FILE_CAPTION = environ.get('FILE_CAPTION', '<code>{file_name}</code>')
 OWNER = int(environ.get('OWNER', '1977238156'))
