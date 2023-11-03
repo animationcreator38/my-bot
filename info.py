f079fac75cf2315a8204303e5c5971abcde1a875
animationcreator38 <147336591+animationcreator38@users.noreply.github.com>
2023-11-03 15:44:46 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,7 +1,7 @@
 from os import environ
 
-API_ID = int(environ.get('API_ID', '882399cfe65fdec68e451e4a6eb246d3'))
-API_HASH = environ.get('API_HASH', '21761864')
+API_ID = int(environ.get('API_ID', '21761864'))
+API_HASH = environ.get('API_HASH', '882399cfe65fdec68e451e4a6eb246d3')
 BOT_TOKEN = environ.get('BOT_TOKEN', '6135291343:AAF4f5HfmjwtTD-Ffk3HuVW_7PYJVEDTKjk')
 FILE_CAPTION = environ.get('FILE_CAPTION', '<code>{file_name}</code>')
 OWNER = int(environ.get('OWNER', '6371417316'))
